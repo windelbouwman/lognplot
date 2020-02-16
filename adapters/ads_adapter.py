baf3eaffc4c06c69f023aa28f713d1910b7c7629
Frits Kuipers <frits.kuipers@gmail.com>
2020-02-16 11:29:22 +0100

    ADS adapter: Process feedback

--- adapters/ads_adapter.py
+++ adapters/ads_adapter.py
@@ -25,20 +25,19 @@ class AdsClient:
         "Entry", ["name", "typename", "comment", "datatype", "datatype_size"]
     )
 
-    # Some data types have same number, maybe use type string field found in __parse_ads_entries...
     DATATYPE_MAP = {
-        17: pyads.PLCTYPE_BOOL,  # ADST_UINT8
-        17: pyads.PLCTYPE_BYTE,  # ADST_UINT8
-        3: pyads.PLCTYPE_DINT,  # ADST_INT32
-        19: pyads.PLCTYPE_DWORD,  # ADST_UINT32
-        2: pyads.PLCTYPE_INT,  # ADST_UINT16
-        5: pyads.PLCTYPE_LREAL,  # ADST_REAL64
-        4: pyads.PLCTYPE_REAL,  # ADST_REAL32
-        16: pyads.PLCTYPE_SINT,  # ADST_INT8
-        19: pyads.PLCTYPE_UDINT,  # ADST_UINT32
-        18: pyads.PLCTYPE_UINT,  # ADST_UINT16
-        17: pyads.PLCTYPE_USINT,  # ADST_UINT8
-        18: pyads.PLCTYPE_WORD,  # ADST_UINT16
+        "BOOL": pyads.PLCTYPE_BOOL,
+        "BYTE": pyads.PLCTYPE_BYTE,
+        "DINT": pyads.PLCTYPE_DINT,
+        "DWORD": pyads.PLCTYPE_DWORD,
+        "INT": pyads.PLCTYPE_INT,
+        "LREAL": pyads.PLCTYPE_LREAL,
+        "REAL": pyads.PLCTYPE_REAL,
+        "SINT": pyads.PLCTYPE_SINT,
+        "UDINT": pyads.PLCTYPE_UDINT,
+        "UINT": pyads.PLCTYPE_UINT,
+        "USINT": pyads.PLCTYPE_USINT,
+        "WORD": pyads.PLCTYPE_WORD,
     }  # type: Dict[str, Type]
 
     def __init__(self, ams_net_id: str, ams_net_port: str):
@@ -52,7 +51,7 @@ class AdsClient:
 
         self.plc.close()
 
-    def start_logging(self, name: str, plc_type):
+    def log(self, name: str, plc_type):
         attr = pyads.NotificationAttrib(ctypes.sizeof(plc_type))
         handles = self.plc.add_device_notification(
             name, attr, self.callback(plc_type), pyads.ADSTRANS_SERVERCYCLE
@@ -64,32 +63,33 @@ class AdsClient:
 
         for entry in parsed_entries:
             if re.match(pattern, entry.name) != None:
-                self.log(entry.name, self.DATATYPE_MAP[entry.datatype])
+                self.log(entry.name, self.DATATYPE_MAP[entry.typename])
 
     def callback(self, plc_type):
         @self.plc.notification(plc_type)
         def decorated_callback(handle, name, timestamp, value):
             client.send_sample(
-                name, self.__format_timestamp(timestamp), float(value),
+                name, AdsClient.format_timestamp(timestamp), float(value),
             )
 
         return decorated_callback
 
     def get_ads_entries(self):
-        upload_info = self.__get_upload_info()
+        upload_info = self._get_upload_info()
         entries = self.plc.read(
             pyads.constants.ADSIGRP_SYM_UPLOAD, 0, ctypes.c_ubyte * upload_info.nSymSize
         )
 
-        parsed_entries = self.__parse_ads_entries(
+        parsed_entries = self._unpack_entries(
             bytes(entries), upload_info.nSymSize, upload_info.nSymbols
         )
         return parsed_entries
 
-    def __format_timestamp(timestamp: datetime):
+    @staticmethod
+    def format_timestamp(timestamp: datetime):
         return time.mktime(timestamp.timetuple()) + timestamp.microsecond / 1e6
 
-    def __get_upload_info(self):
+    def _get_upload_info(self):
         upload_info = self.plc.read(
             pyads.constants.ADSIGRP_SYM_UPLOADINFO,
             0,
@@ -97,40 +97,46 @@ class AdsClient:
         )
         return upload_info
 
-    def __parse_ads_entries(self, entries_data: bytes, size: int, count: int):
+    def _unpack_entry(data: bytes, index: int) -> (int, Entry):
+        fmt = "<6I3H"
+        (
+            entry_length,
+            _,
+            _,
+            entry_datatype_size,
+            entry_datatype,
+            _,
+            name_len,
+            type_len,
+            comment_len,
+        ) = struct.unpack_from(fmt, entries_data, index)
+
+        name_offset = index + struct.calcsize(fmt)
+        name = entries_data[name_offset : name_offset + name_len].decode("ascii")
+
+        type_offset = name_offset + name_len + 1
+        typ = entries_data[type_offset : type_offset + type_len].decode("ascii")
+
+        comment_offset = type_offset + type_len + 1
+        comment = entries_data[comment_offset : comment_offset + comment_len].decode(
+            "ascii"
+        )
+
+        return (
+            entry_length,
+            self.Entry(name, typ, comment, entry_datatype, entry_datatype_size),
+        )
+
+    def _unpack_entries(self, entries_data: bytes, size: int, count: int):
         parsed_entries = []
-        total_bytes = 0
-        while total_bytes < size:
-            fmt = "<6I3H"
-            (
-                entry_length,
-                _,
-                _,
-                entry_datatype_size,
-                entry_datatype,
-                _,
-                name_len,
-                type_len,
-                comment_len,
-            ) = struct.unpack_from(fmt, entries_data, total_bytes)
-
-            name_offset = total_bytes + struct.calcsize(fmt)
-            name = entries_data[name_offset : name_offset + name_len].decode("ascii")
-
-            type_offset = name_offset + name_len + 1
-            typ = entries_data[type_offset : type_offset + type_len].decode("ascii")
-
-            comment_offset = type_offset + type_len + 1
-            comment = entries_data[
-                comment_offset : comment_offset + comment_len
-            ].decode("ascii")
-
-            total_bytes += entry_length
-            parsed_entries.append(
-                self.Entry(name, typ, comment, entry_datatype, entry_datatype_size)
-            )
+        index = 0
+        while index < size:
+            entry_size, entry = _unpack_entry(data, index)
+            index += entry_size
+            parsed_entries.append(entry)
 
         assert len(parsed_entries) == count
+        assert index == size
         return parsed_entries
 
 
@@ -161,7 +167,7 @@ def main():
     ads_client = AdsClient(args.ams_net_id, args.ams_net_port)
 
     ads_client.subscribe(args.regex)
-    ads_client.start_logging("GVL.test", pyads.PLCTYPE_LREAL)
+    ads_client.log("GVL.test", pyads.PLCTYPE_LREAL)
 
 
 if __name__ == "__main__":
